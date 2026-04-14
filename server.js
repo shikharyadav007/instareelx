18e904fe9357ef56f743e20cfc09f7ca2de1c011
Shikhar Yadav <shikharyadav@Shikhars-MacBook-Pro.local>
2026-04-14 15:51:29 +0530

    fixed structure

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3000;
 
-// IMPORTANT: correct path serve karo
 app.use(express.static(path.join(__dirname)));
 
-// Home route (VERY IMPORTANT)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Download route
 app.get("/download-file", (req, res) => {
   const fileUrl = req.query.url;
 
